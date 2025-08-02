034d721ee90c372f3b4da0e1d6e6985b800c1d81
zhonghui-wu <1187338689@qq.com>
2025-08-02 22:21:51 +0800

    完善main.py,可执行所有用例

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# 这是一个示例 Python 脚本。
+import pytest
+from utils.handle_path import report_path
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-
-
-# 按装订区域中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+    # 执行所有用例
+    # -v：显示详细信息
+    # -s：显示print内容
+    # --alluredir：生成allure报告目录（可选）
+    pytest.main(['-vs', './testcase','--clean-alluredir', '--alluredir', report_path])
